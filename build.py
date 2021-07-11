141e05e42caf3c418e5d98e01396db5967cd469f
Mitch Brandon <mitchell.roy.brandon@gmail.com>
2021-07-11 11:08:34 -0700

    built a for loop that runs through a long messy if statement refactoring code and executing my templating. this is the first commit of the new code

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 # phase1: refactor this code into a main() function
+#phase2: write a forloop that loops through the list/dict and executes the templating
 def main():
     print('building ssg')
     pages = [{
@@ -16,33 +17,63 @@ def main():
         'output': 'docs/contact.html',
         'title': 'Contact',
     }]
-
-    for page in pages:
-        title = page['title']
-        file_name = page['filename']
-        output = page['output']
-
-
-# variables that read top and bottom html
+    # variables that read top and bottom html
     top_html = open('templates/top.html').read()
     bottom_html = open('templates/bottom.html').read()
 
-#variables that read the middle contents of html
-    mid_index_html = open('contents/index.html').read()
-    mid_blog_html = open('contents/blog.html').read()
-    mid_contact_html = open('contents/contact.html').read()
+    for page in pages:
+        title = page['title']
+        filename = page['filename']
+        output = page['output']
 
-# combining tops + html middle contents + bottoms
-    index_html = top_html + mid_index_html + bottom_html
-    blog_html = top_html + mid_blog_html + bottom_html
-    contact_html = top_html + mid_contact_html + bottom_html
+        if filename == 'contents/index.html' and title == 'About' and output == 'docs/index.html':
 
-#writing/creating the new html pages: stored in html directory
-    open('docs/index.html', 'w+').write(index_html)
-    open('docs/blog.html', 'w+').write(blog_html)
-    open('docs/contact.html', 'w+').write(contact_html)
+            print('filename =', filename, 'title =', title, 'output =', output)
+            mid_index_html = open(filename).read()
+            index_html = top_html + mid_index_html + bottom_html
+            # open('docs/index.html', 'w+').write(output)
+            open(output, 'w+').write(index_html)
 
+        elif filename == 'contents/blog.html' and title == 'Blog' and output == 'docs/blog.html':
+
+            print('filename =', filename, 'title =', title, 'output =', output)
+            mid_blog_html = open(filename).read()
+            blog_html = top_html + mid_blog_html + bottom_html
+            # open('docs/blog.html', 'w+').write(output)
+            open(output, 'w+').write(blog_html)
+
+        elif filename == 'contents/contact.html' and title == 'Contact' and output == 'docs/contact.html':
+
+            print('filename =', filename, 'title =', title, 'output =', output)
+            mid_contact_html = open(filename).read()
+            contact_html = top_html + mid_contact_html + bottom_html
+            # open('docs/contact.html', 'w+').write(output)
+            open(output, 'w+').write(contact_html)
+        else: pass
 
 main()
 
-# phase1 of main() is currently is currently functioning
+
+
+
+# variables that read top and bottom html
+    # top_html = open('templates/top.html').read()
+    # bottom_html = open('templates/bottom.html').read()
+
+#variables that read the middle contents of html
+    # mid_index_html = open('contents/index.html').read()
+    # mid_blog_html = open('contents/blog.html').read()
+    # mid_contact_html = open('contents/contact.html').read()    
+
+# combining tops + html middle contents + bottoms
+    # index_html = top_html + mid_index_html + bottom_html
+    # blog_html = top_html + mid_blog_html + bottom_html
+    # contact_html = top_html + mid_contact_html + bottom_html
+
+#writing/creating the new html pages: stored in html directory, this is the output
+    # open('docs/index.html', 'w+').write(index_html)
+    # open('docs/blog.html', 'w+').write(blog_html)
+    # open('docs/contact.html', 'w+').write(contact_html)
+
+
+
