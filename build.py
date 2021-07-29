f83cc67dc2d267683bd0d1db4b81e70be8853543
Mitch Brandon <mitchell.roy.brandon@gmail.com>
2021-07-29 05:54:10 -0700

    deletes the get_pages() func in build.py and refactors the main() func to auto detect templating files. templating is working properly. this initial refactor is successful

--- build.py
+++ build.py
@@ -2,19 +2,18 @@ import glob
 import os
 import pprint
 
-# def pages():
-#     return []
+
     
 def write_page(title, filename, output):
     template = open('templates/base.html').read()
     content = open(filename).read()
     combined_content = template.replace('{{content}}', content).replace('{{title}}', title)
 
-    if title == 'About':
+    if title == 'ABOUT':
         combined_content = combined_content.replace('{{about_xtra_class}}', 'active').replace('{{linkedin}}', 'about-linkedin').replace('{{github}}', 'about-github')
-    elif title == 'Blog':
+    elif title == 'BLOG':
         combined_content = combined_content.replace('{{blog_xtra_class}}', 'active').replace('{{linkedin}}', 'blog-linkedin').replace('{{github}}', 'blog-github')
-    elif title == 'Contact':
+    elif title == 'CONTACT':
         combined_content = combined_content.replace('{{contact_xtra_class}}', 'active').replace('{{linkedin}}', 'contact-linkedin').replace('{{github}}', 'contact-github')
     else: pass
 
@@ -30,7 +29,7 @@ def main():
     for files, file in zip(all_html_files, all_output_files):
         filename = os.path.relpath(files)
         output = os.path.relpath(file)
-        title_parse = os.path.basename(filename)
+        title_parse = os.path.basename(filename).upper()
         title, extension = os.path.splitext(title_parse)
 
         pages.append({
@@ -38,5 +37,10 @@ def main():
             'output': output,
             'title': title,
         })
+
+        if title == 'INDEX':
+            title = 'ABOUT'
+
+        write_page(title, filename, output)
     pprint.pprint(pages, indent = 2, width = 30)
 main()
