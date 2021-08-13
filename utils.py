3a6f1f2548302b310a276b6af046f61d8621571e
Mitch Brandon <mitchell.roy.brandon@gmail.com>
2021-08-13 06:11:46 -0700

    first commit of the testing branch. this commit solves the problem of populating the ordering of the pages[] list. see function getPageOrder

--- utils.py
+++ utils.py
@@ -7,39 +7,55 @@ from jinja2 import Template
 def write_page(title, filename, output, pages):
     template_html = open('templates/base.html').read()
     template = Template(template_html)
-    print('these are the outputs:', output)
+
     result = template.render(
         pages_list = pages,
         current_page_title = title,
         html_contents = open(filename).read()
     )
-    #print('these are the outputs:', output)
+
     open(output, 'w+').write(result)
+
+def getPageOrder(page):
+    return page['order']
 
 def main():
     all_html_files = glob.glob('contents/*.html')
-    # just_the_filename = 'about.html' # or from a os.path method...?
-    # output_filename = 'docs/' + just_the_filename 
     pages=[]
+    current_order = 1
     for files in all_html_files:
         filename = os.path.relpath(files)
         clipped_filename = os.path.basename(filename)
         output = 'docs/' + clipped_filename
         title_parse = os.path.basename(filename).upper()
         title, extension = os.path.splitext(title_parse)
+        
         if title == 'INDEX':
             title = 'ABOUT'
+            order = 0
+        elif title == 'CONTACT':
+            order = len(all_html_files) - 1
+        else:
+            order = current_order
+            current_order += 1
 
         pages.append({
             'filename': filename,
             'output': output,
             'title': title,
             'clipped_filename': clipped_filename,
+            'order': order
         })
     
     #sort pages into new sort
-    page_sort = [1,0,2,3]
-    pages = [pages[i] for i in page_sort]
+    '''
+    page_sort = [1,0,2]
+    if (len(pages) >= 3):
+        for i in range(3, len(pages)):
+            page_sort.append(i)
+    '''
+
+    pages.sort(key=getPageOrder)
 
     for page in pages:
         title = page['title']
